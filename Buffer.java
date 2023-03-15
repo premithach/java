e8789bf43c38fb6421a6603d4bd951cb8a191f64
Premitha Ch <102718766+premithach@users.noreply.github.com>
2023-03-15 22:55:39 +0530

    Add files via upload

---
+++ Buffer.java
@@ -0,0 +1,9 @@
+class Buffer
+ {  
+    public static void main(String args[])
+	{  
+        StringBuffer buffer=new StringBuffer("hello");  
+        buffer.append("java");  
+        System.out.println(buffer);  
+    }  
+}
---
+++ Builder.java
@@ -0,0 +1,9 @@
+class Builder
+{  
+    public static void main(String args[])
+	{  
+        StringBuilder builder=new StringBuilder("hello");  
+        builder.append("java");  
+        System.out.println(builder);  
+    }  
+}  
